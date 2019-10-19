00ad6d36f5a7f2989db4223b949432170a1a3272
Daanish Khan <dkhan045@uOttawa.ca>
2019-10-19 15:48:12 -0400

    Fiddling around with particles

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 //PARTICLES ORANGE//
-particlesJS("particleCanvas-Orange",
+particlesJS("bgParticles",
 {
   "particles": {
     "number": {
-      "value": 120,
+      "value": 355,
       "density": {
         "enable": true,
         "value_area": 800
       }
     },
     "color": {
-      "value": "#ec543f"
+      "value": "#5eff00"
     },
     "shape": {
       "type": "circle",
@@ -19,7 +19,7 @@ particlesJS("particleCanvas-Orange",
         "color": "#000000"
       },
       "polygon": {
-        "nb_sides": 3
+        "nb_sides": 5
       },
       "image": {
         "src": "img/github.svg",
@@ -28,149 +28,35 @@ particlesJS("particleCanvas-Orange",
       }
     },
     "opacity": {
-      "value": 0.5,
-      "random": false,
-      "anim": {
-        "enable": true,
-        "speed": 1,
-        "opacity_min": 0.1,
-        "sync": false
-      }
-    },
-    "size": {
-      "value": 20,
+      "value": 1,
       "random": true,
       "anim": {
-        "enable": false,
-        "speed": 10,
-        "size_min": 0.1,
-        "sync": false
-      }
-    },
-    "line_linked": {
-      "enable": false,
-      "distance": 150,
-      "color": "#ffffff",
-      "opacity": 0.4,
-      "width": 1
-    },
-    "move": {
-      "enable": true,
-      "speed": 0.5,
-      "direction": "none",
-      "random": true,
-      "straight": false,
-      "out_mode": "bounce",
-      "bounce": false,
-      "attract": {
-        "enable": false,
-        "rotateX": 394.57382081613633,
-        "rotateY": 157.82952832645452
-      }
-    }
-  },
-  "interactivity": {
-    "detect_on": "canvas",
-    "events": {
-      "onhover": {
         "enable": true,
-        "mode": "grab"
-      },
-      "onclick": {
-        "enable": false,
-        "mode": "push"
-      },
-      "resize": true
-    },
-    "modes": {
-      "grab": {
-        "distance": 200,
-        "line_linked": {
-          "opacity": 0.2
-        }
-      },
-      "bubble": {
-        "distance": 1500,
-        "size": 40,
-        "duration": 7.272727272727273,
-        "opacity": 0.3676323676323676,
-        "speed": 3
-      },
-      "repulse": {
-        "distance": 50,
-        "duration": 0.4
-      },
-      "push": {
-        "particles_nb": 4
-      },
-      "remove": {
-        "particles_nb": 2
-      }
-    }
-  },
-  "retina_detect": true
-});
-
-
-//PARTICLES BLUE//
-particlesJS("particleCanvas-Blue",
-{
-  "particles": {
-    "number": {
-      "value": 80,
-      "density": {
-        "enable": true,
-        "value_area": 800
-      }
-    },
-    "color": {
-      "value": "#ffffff"
-    },
-    "shape": {
-      "type": "circle",
-      "stroke": {
-        "width": 0,
-        "color": "#000000"
-      },
-      "polygon": {
-        "nb_sides": 3
-      },
-      "image": {
-        "src": "img/github.svg",
-        "width": 100,
-        "height": 100
-      }
-    },
-    "opacity": {
-      "value": 0.5,
-      "random": true,
-      "anim": {
-        "enable": false,
-        "speed": 0.2,
+        "speed": 0.32460945425035526,
         "opacity_min": 0,
         "sync": false
       }
     },
     "size": {
-      "value": 15,
+      "value": 8.011985930952697,
       "random": true,
       "anim": {
-        "enable": true,
-        "speed": 10,
-        "size_min": 0.1,
+        "enable": false,
+        "speed": 4,
+        "size_min": 0.3,
         "sync": false
       }
     },
     "line_linked": {
-      "enable": false,
-      "distance": 150,
+      "enable": true,
+      "distance": 80.11985930952699,
       "color": "#ffffff",
-      "opacity": 0.4,
+      "opacity": 0.20029964827381747,
       "width": 1
     },
     "move": {
       "enable": true,
-      "speed": 0.5,
+      "speed": 1,
       "direction": "none",
       "random": true,
       "straight": false,
@@ -178,8 +64,8 @@ particlesJS("particleCanvas-Blue",
       "bounce": false,
       "attract": {
         "enable": true,
-        "rotateX": 3945.7382081613637,
-        "rotateY": 157.82952832645452
+        "rotateX": 2643.9553572143905,
+        "rotateY": 961.4383117143238
       }
     }
   },
@@ -187,31 +73,31 @@ particlesJS("particleCanvas-Blue",
     "detect_on": "canvas",
     "events": {
       "onhover": {
-        "enable": false,
-        "mode": "grab"
+        "enable": true,
+        "mode": "bubble"
       },
       "onclick": {
         "enable": false,
-        "mode": "push"
+        "mode": "repulse"
       },
       "resize": true
     },
     "modes": {
       "grab": {
-        "distance": 200,
+        "distance": 119.88011988011988,
         "line_linked": {
-          "opacity": 0.2
+          "opacity": 1
         }
       },
       "bubble": {
-        "distance": 1500,
-        "size": 40,
-        "duration": 7.272727272727273,
-        "opacity": 0.3676323676323676,
+        "distance": 109.5556908094949,
+        "size": 0,
+        "duration": 0.9738283627510659,
+        "opacity": 0,
         "speed": 3
       },
       "repulse": {
-        "distance": 50,
+        "distance": 154.18949076891874,
         "duration": 0.4
       },
       "push": {
@@ -223,5 +109,4 @@ particlesJS("particleCanvas-Blue",
     }
   },
   "retina_detect": true
-});
- 
+})
